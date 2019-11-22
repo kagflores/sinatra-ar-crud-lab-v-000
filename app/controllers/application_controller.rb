7f0bed1713d2488308869b4b57362e1b3f4ae8dc
Karen Flores <karen.ag.flores@gmail.com>
2019-11-22 22:35:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
     @article = Article.find(params[:id])
     @article.update(params[:article])
-    erb :show
+    redirect to '/articles/#{ @article.id }'
   end
 
   delete '/articles/:id' do
